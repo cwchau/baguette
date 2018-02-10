5c3cf45073a372465efade1632219f08f9bac88e
Melodee Li <melodee@247teach.org>
2018-02-10 09:16:09 -0500

    barcode generator works with button

--- app/src/main/java/com/example/firec/baguette/Barcode.java
+++ app/src/main/java/com/example/firec/baguette/Barcode.java
@@ -3,14 +3,30 @@ package com.example.firec.baguette;
 /**
  * Created by firec on 2/10/2018.
  */
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class Barcode extends Fragment {
+    ImageView imageView;
+    Button button;
+    EditText editText;
+    String EditTextValue;
+    Thread thread;
+    public final static int QRcodeWidth = 500;
+    Bitmap bitmap;
 
     @Nullable
     @Override
@@ -26,5 +42,53 @@ public class Barcode extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Barcode");
+
+        imageView = (ImageView) view.findViewById(R.id.imageView);
+        //editText = (EditText) view.findViewById(R.id.editText);
+        button = (Button) view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditTextValue = "username"; //todo: edit to actual username
+                try {
+                    bitmap = TextToImageEncode(EditTextValue);
+                    imageView.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+        } catch (IllegalArgumentException Illegalargumentexception) {
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                    getResources().getColor(R.color.primary_text_default_material_light) : getResources().getColor(R.color.primary_text_default_material_dark);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
     }
 }
